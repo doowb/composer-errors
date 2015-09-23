32d6e0a3542f68351ed316c578cb2d02c87d5967
Brian Woodward <brian.woodward@gmail.com>
2015-09-23 00:56:31 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * composer-error <https://github.com/doowb/composer-error>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
